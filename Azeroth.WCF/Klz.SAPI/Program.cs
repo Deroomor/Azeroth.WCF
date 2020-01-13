2ce52153b76095b02473fbcd72d261cb01778f52
Deroomor <eedroom@163.com>
2020-01-13 22:06:25 +0800

    wcf使用证书

--- Azeroth.WCF/Klz.SAPI/Program.cs
+++ Azeroth.WCF/Klz.SAPI/Program.cs
@@ -9,6 +9,11 @@ namespace Klz.SAPI
     {
         static void Main(string[] args)
         {
+            var host= new System.ServiceModel.ServiceHost(typeof(UserInfo));
+            host.Open();
+            Console.WriteLine("http://localhost:8028/UserInfo");
+            Console.ReadLine();
+            host.Close();
         }
     }
 }
--- Azeroth.WCF/Klz.SAPI/UserInfo.cs
+++ Azeroth.WCF/Klz.SAPI/UserInfo.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Klz.SAPI.Model;
 
 namespace Klz.SAPI
 {
-    class UserInfo
+    public class UserInfo : IUserInfo
     {
+        public Model.UserInfo Add(Model.UserInfo model)
+        {
+            return model;
+        }
+
+        public List<Model.UserInfo> GetList(Model.UserInfo predicate)
+        {
+            return new List<Model.UserInfo>() { predicate };
+        }
     }
 }
