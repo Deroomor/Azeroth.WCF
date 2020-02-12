af7171f2613a2de728b7eccd8a8b763af3383cc1
Deroomor <eedroom@163.com>
2020-02-12 23:54:30 +0800

    增加使用证书的例子

---
+++ Klz.Cert.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace Klz.Cert.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var factory = new System.ServiceModel.ChannelFactory<IHomeHandler>("wch"))
+            {
+                var client = factory.CreateChannel();
+
+                var rt = client.DoWork();
+            }
+        }
+    }
+
+    [System.ServiceModel.ServiceContract]
+    public interface IHomeHandler
+    {
+        [OperationContract]
+        string DoWork();
+    }
+}
